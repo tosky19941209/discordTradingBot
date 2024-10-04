29cc4c09d282fc9475a826fc4cccbd07f39d425b
tosky19941209 <felixeliass1994@gmail.com>
2024-10-04 18:10:07 +0900

    back-end

--- src/service/index.tsx
+++ src/service/index.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "http://18.226.98.189:8080"
-    baseURL: "http://78.46.76.71:8080/"
+    baseURL: "http://18.226.98.189:8080"
+    // baseURL: "http://78.46.76.71:8080/"
 })
 
 export default api
