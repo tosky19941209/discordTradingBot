94b932a27b8d851d1d1ba591dc1c3e2d520b35b1
tosky19941209 <felixeliass1994@gmail.com>
2024-10-15 04:52:04 +0900

    feature: new password changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           <>
             <div className="fixed top-0 left-0 w-[100%] h-[100%] bg-[white] opacity-50 z-[10] blur-5xl" >
             </div>
-            <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 sm:w-[500px] w-[90%] bg-[black] z-[14]">
+            <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 sm:w-[500px] w-[90%] bg-[black] z-[14] rounded-xl">
               <Modal />
             </div>
           </>
